659b52e76b649cce8b4e382e9de6421e141167fc
Duc Le <leminhducktvn@gmail.com>
2016-07-12 18:50:33 +0800

    Fix a bug

--- clear_images.sh
+++ clear_images.sh
@@ -4,8 +4,9 @@ PROJECT_LATEST_TAG=temp_proj
 file="/home/$(whoami)/autoserver/flag.on"
 
 if [ -f "$file" ]
-  exit 1
 then
+  exit 1
+else
   latest_id=$(docker images | grep $PROJECT_LATEST_TAG | awk {'print $3'})
   docker rmi $(docker images | grep -v $latest_id | awk {'print $3'} | tail -n +2)
 fi
